33c6072f615f0665195a92fb355f5360351939c4
Andrey Prokopenko <persiantiger@yandex.ru>
2025-10-14 15:16:06 +0200

    feat: disable webhook and switch to long polling

--- src/Watcher/Bot.hs
+++ src/Watcher/Bot.hs
@@ -166,11 +166,11 @@ runTelegramBot st@BotState{..} = do
   let ?model = st
   let Settings{..} = botSettings
       startBot' = case communication of
-        LongPolling -> startBotAsync
+        LongPolling -> startBotAsync'
         Webhook webhookCfg -> startBotWebhookAsync' webhookCfg
 
       startWebhookAsync bot WebhookConfig{..} env = do
-        botEnv <- deleteWebhook env >> startBotEnv bot env
+        botEnv <- startBotEnv bot env
         setUpWebhook webhookConfigSetWebhookRequest env >>= \case
           Left err -> error $ show err
           Right _ -> fork_ $ do
@@ -196,6 +196,8 @@ runTelegramBot st@BotState{..} = do
               , webhookConfigSetWebhookRequest = requestData
               }
         startWebhookAsync bot cfg env
+
+      startBotAsync' bot env = deleteWebhook env >> startBotAsync bot env
 
   botActionFun <- startBot' watcherBot clientEnv
   runConcurrently $
